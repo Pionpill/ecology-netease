e64b0c8852b2de7b520168da505139964e42cc52
pionpill <673486387@qq.com>
2024-04-14 18:42:06 +0800

    :label: 属性名规范

--- ecology_behavior_pack/scripts/ecology/server/service/BiomeService.py
+++ ecology_behavior_pack/scripts/ecology/server/service/BiomeService.py
@@ -21,30 +21,30 @@ class BiomeService(object):
     # MC 生存模式中间高度
     MIDDLE_HEIGHT = 128
 
-    EcologyMgrDict = {} # type: dict[tuple[int, int, int, int], tuple[EcologyManager, int]]
+    ecologyMgrDict = {} # type: dict[tuple[int, int, int, int], tuple[EcologyManager, int]]
 
     @staticmethod
     def GetEcologyManager(position, dimensionId):
         # type: (tuple[int, int, int], int) -> EcologyManager
         posKey = position + (dimensionId,)
-        ecologyMgrTuple = BiomeService.EcologyMgrDict.get(posKey)
+        ecologyMgrTuple = BiomeService.ecologyMgrDict.get(posKey)
         if ecologyMgrTuple is not None:
-            BiomeService.EcologyMgrDict[posKey] = (ecologyMgrTuple[0], int(time.time()))
+            BiomeService.ecologyMgrDict[posKey] = (ecologyMgrTuple[0], int(time.time()))
             BiomeService.__RefreshEcologyMgrDict()
             return ecologyMgrTuple[0]
         ecologyManager = EcologyManager(position, dimensionId)
-        BiomeService.EcologyMgrDict[posKey] = (ecologyManager, int(time.time()))
+        BiomeService.ecologyMgrDict[posKey] = (ecologyManager, int(time.time()))
         return ecologyManager
     
     @staticmethod
     def __RefreshEcologyMgrDict():
         """⚡刷新生态管理表"""
-        for posKey, ecologyMgrTuple in BiomeService.EcologyMgrDict.items():
+        for posKey, ecologyMgrTuple in BiomeService.ecologyMgrDict.items():
             now = int(time.time())
             lastGetTime = ecologyMgrTuple[1]
             # 超过 10 分钟就删除管理类
             if now - lastGetTime > 600:
-                del BiomeService.EcologyMgrDict[posKey]
+                del BiomeService.ecologyMgrDict[posKey]
 
     @staticmethod
     def GetBiomeInfo(position, dimensionId):
