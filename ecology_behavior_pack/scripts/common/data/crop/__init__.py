04cbfc67ac6e0872581da7925cd18e3ba881c006
pionpill <673486387@qq.com>
2024-04-18 01:09:22 +0800

    :seedling: 添加作物：玉米

--- ecology_behavior_pack/scripts/common/data/crop/__init__.py
+++ ecology_behavior_pack/scripts/common/data/crop/__init__.py
@@ -1,7 +1,9 @@
+from scripts.common.data.crop.corn import CORN
 from scripts.common.data.crop.herb import HERB
 
 CROP_DATA = {
-    "ham:herb": HERB
+    "ham:herb": HERB,
+    "ham:corn": CORN
 }
 
 __all__ = [CROP_DATA]
---
+++ ecology_behavior_pack/scripts/common/data/crop/corn.py
@@ -0,0 +1,54 @@
+CORN = {
+    "beta": True,
+    "seed": 'ham:corn',
+    "blockPrefix": 'ham:corn',
+    "grow": {
+        "stage": (
+            {
+                "tick": 4,
+                "height": (26, 32)
+            },
+            {
+                "tick": 4,
+                "height": (42, 32)
+            },
+            {
+                "tick": 6,
+                "height": (64, 32)
+            },
+            {
+                "height": (70, 32)
+            }
+        ),
+        "harvest": {
+            "count": 1,
+            "stage": 3
+        },
+        "temperature": {
+            "suit": (15, 30),
+            "can": (5, 40)
+        },
+        "rainfall": {
+            "suit": (40, 100),
+            "can": (15, 100)
+        },
+        "brightness": {
+            "suit": (9, 15),
+            "can": (5, 15)
+        },
+        "fertility": {
+            "min": 100,
+            "sensitivity": 110,
+            "type": ["dirt"]
+        },
+        "rain_multiply": 1.5,
+        "period": "sun"
+    },
+    "loot": (
+        {
+            "name": "ham:corn",
+            "chance": 100,
+            "count": 4
+        },
+    )
+}
