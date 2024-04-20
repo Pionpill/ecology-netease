5dc18704aea82509acf7129a458dc250d3abf7b7
pionpill <673486387@qq.com>
2024-04-20 22:45:58 +0800

    :seedling: 添加作物：生菜

--- ecology_behavior_pack/scripts/common/data/crop/__init__.py
+++ ecology_behavior_pack/scripts/common/data/crop/__init__.py
@@ -1,11 +1,13 @@
 from scripts.common.data.crop.corn import CORN
 from scripts.common.data.crop.eggplant import EGGPLANT
 from scripts.common.data.crop.herb import HERB
+from scripts.common.data.crop.lettuce import LETTUCE
 
 CROP_DATA = {
     "ham:herb": HERB,
     "ham:corn": CORN,
-    "ham:eggplant": EGGPLANT
+    "ham:eggplant": EGGPLANT,
+    "ham:lettuce": LETTUCE,
 }
 
 __all__ = [CROP_DATA]
---
+++ ecology_behavior_pack/scripts/common/data/crop/lettuce.py
@@ -0,0 +1,50 @@
+LETTUCE = {
+    "beta": True,
+    "seed": 'ham:lettuce',
+    "blockPrefix": 'ham:lettuce',
+    "grow": {
+        "stage": (
+            {
+                "tick": 11,
+                "height": (12, 32)
+            },
+            {
+                "tick": 11,
+                "height": (13, 32)
+            },
+            {
+                "height": (14, 32)
+            }
+        ),
+        "harvest": {
+            "count": 1,
+            "stage": 2,
+        },
+        "temperature": {
+            "suit": (10, 25),
+            "can": (0, 30)
+        },
+        "rainfall": {
+            "suit": (60, 100),
+            "can": (30, 100)
+        },
+        "brightness": {
+            "suit": (6, 12),
+            "can": (4, 20)
+        },
+        "fertility": {
+            "min": 80,
+            "sensitivity": 50,
+            "type": ["dirt"]
+        },
+        "rain_multiply": 1.5,
+        "period": "none"
+    },
+    "loot": (
+        {
+            "name": "ham:lettuce",
+            "chance": 100,
+            "count": 4
+        },
+    )
+}
