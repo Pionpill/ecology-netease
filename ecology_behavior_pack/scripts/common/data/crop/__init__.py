22ad5d6172856f2347899ddfee0029d8ddfed983
pionpill <673486387@qq.com>
2024-04-20 23:54:36 +0800

    :seedling: 添加作物：洋葱

--- ecology_behavior_pack/scripts/common/data/crop/__init__.py
+++ ecology_behavior_pack/scripts/common/data/crop/__init__.py
@@ -2,12 +2,14 @@ from scripts.common.data.crop.corn import CORN
 from scripts.common.data.crop.eggplant import EGGPLANT
 from scripts.common.data.crop.herb import HERB
 from scripts.common.data.crop.lettuce import LETTUCE
+from scripts.common.data.crop.onion import ONION
 
 CROP_DATA = {
     "ham:herb": HERB,
     "ham:corn": CORN,
     "ham:eggplant": EGGPLANT,
     "ham:lettuce": LETTUCE,
+    "ham:onion": ONION,
 }
 
 __all__ = [CROP_DATA]
---
+++ ecology_behavior_pack/scripts/common/data/crop/onion.py
@@ -0,0 +1,54 @@
+ONION = {
+    "beta": True,
+    "seed": 'ham:onion',
+    "blockPrefix": 'ham:onion',
+    "grow": {
+        "stage": (
+            {
+                "tick": 55,
+                "height": (103, 32)
+            },
+            {
+                "tick": 55,
+                "height": (14, 32)
+            },
+            {
+                "tick": 56,
+                "height": (20, 32)
+            },
+            {
+                "height": (22, 32)
+            }
+        ),
+        "harvest": {
+            "count": 1,
+            "stage": 3,
+        },
+        "temperature": {
+            "suit": (10, 25),
+            "can": (-5, 35)
+        },
+        "rainfall": {
+            "suit": (40, 80),
+            "can": (15, 100)
+        },
+        "brightness": {
+            "suit": (9, 15),
+            "can": (5, 15)
+        },
+        "fertility": {
+            "min": 60,
+            "sensitivity": 40,
+            "type": ["dirt"]
+        },
+        "rain_multiply": 1.5,
+        "period": "sun"
+    },
+    "loot": (
+        {
+            "name": "ham:onion",
+            "chance": 100,
+            "count": 10
+        },
+    )
+}
