d2d36bc9597bead2f42e78975090d20dce24069b
pionpill <673486387@qq.com>
2024-05-06 00:59:50 +0800

    :scroll: 添加配方：肉糜

--- ecology_behavior_pack/scripts/common/data/workbench/recipe/cook.py
+++ ecology_behavior_pack/scripts/common/data/workbench/recipe/cook.py
@@ -5,5 +5,15 @@ COOK_DATA = {
         },
         "fixed_material": (1, 0, 0, 0),
         "result": "minecraft:apple",
+    },
+    "ham:ground_pork": {
+        "material": {
+            4: "minecraft:porkchop"
+        },
+        "fixed_material": (1, 0, 0, 1),
+        "result": {
+            "newItemName": "ham:ground_pork", 
+            "count": 2,
+        },
     }
 }
