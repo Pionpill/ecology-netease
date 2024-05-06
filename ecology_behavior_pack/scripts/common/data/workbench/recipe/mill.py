a65bf4c79f624a738a08741cf2bf149b297b0e5e
pionpill <673486387@qq.com>
2024-05-07 00:39:13 +0800

    :scroll: 添加石磨配方

--- ecology_behavior_pack/scripts/common/data/workbench/recipe/mill.py
+++ ecology_behavior_pack/scripts/common/data/workbench/recipe/mill.py
@@ -1,58 +1,48 @@
 MILL_DATA = {
     "minecraft:apple": "minecraft:apple",
-    "ham:spices": {
+    "ham:seasoning": {
         "material": {
-            0: {
-                "newItemName": "ham:herb",
-                "count": 3
-            }
+            "newItemName": "ham:herb",
+            "count": 3,
         },
         "result": {
-            0: "ham:spices",
-            1: "ham:straw"
-        }
-    },
-    "ham:rice_pieces": {
-        "material": {
             0: {
-                "newItemName": "ham:rice",
-                "count": 3
-            }
-        },
-        "result": {
-            0: "ham:rice_pieces",
-            1: "ham:straw",
-        }
-    },
-    "minecraft:flour": {
-        "material": {
-            0: {
-                "newItemName": "minecraft:wheat",
-                "count": 3
-            }
-        },
-        "result": {
-            0: "ham:flour",
-            1: "ham:straw"
-        }
-    },
-    "ham:corn_pieces": {
-        "material": {
-            0: {
-                "newItemName": "ham:corn",
+                "newItemName": "ham:seasoning",
                 "count": 3,
-            }
-        },
-        "result": {
-            0: "ham:corn_pieces"
+            },
+            1: "ham:straw"
         }
     },
-    "ham:chill_powder": {
+    "ham:chili_powder": {
         "material": {
             0: "ham:chili"
         },
         "result": {
-            0: "ham:chill_powder"
+            0: "ham:chili_powder"
+        }
+    },
+    "ham:rice": {
+        "material": "ham:rice_plant",
+        "result": {
+            0: {
+                "newItemName": "ham:rice",
+                "count": 2,
+            },
+            1: "ham:straw",
+        }
+    },
+    "ham:starch": {
+        "material": "ham:rice",
+        "result": {
+            "newItemName": "ham:starch",
+            "count": 2,
+        }
+    },
+    "ham:flour": {
+        "material": "minecraft:wheat",
+        "result": {
+            "newItemName": "ham:flour",
+            "count": 1,
         }
     }
 }
